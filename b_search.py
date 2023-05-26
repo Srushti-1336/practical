6e94e4d8131a09f084fa0b055c2a4714ba58060d
Srushti-1336 <127395330+Srushti-1336@users.noreply.github.com>
2023-05-26 11:46:49 +0530

    Update b_search.py

--- b_search.py
+++ b_search.py
@@ -2,25 +2,24 @@ def binary_search(arr,target):
   low=0
   high=len(arr)-1
   
-  while low <=high:
-    mid = (low+high)
+  while low<=high:
+    mid=(low+high)
     guess=arr[mid]
     
     if guess == target:
       return mid
-    elif guess< target:
-      low = mid+1
+    elif guess < target:
+      low=mid+1
       else:
-        high=mid-1
-return -1 #target not found
+        high=nid-1
+        return -1
       
-      
-numbers=[2,3,8,4,5,9.7,1]
+numbers=[2,5,34,6,4,7,3,88,10,1]
 target_number=5
-      
-result = binary_search(numbers,target_number)
-      
-if result i=-1:
-  print("target found at index",result)
-  else:
-    print("target not found")
+
+result=binary_search(numbers,target_number)
+if result i=-1;
+print("Target Found at index",result)
+
+else:
+  print("Target not found")
